3be3ffe0f38952f59568ff116437b5ab57f689c8
louisaldorio <louisaldorio@gmail.com>
2021-05-06 21:28:53 +0700

    image internal storage

--- app/src/main/java/com/example/inception/fragment/ScheduleFragment.kt
+++ app/src/main/java/com/example/inception/fragment/ScheduleFragment.kt
@@ -1,69 +1,59 @@
 package com.example.inception.fragment
 
-import android.app.AlarmManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.os.Build
+import android.content.ContextWrapper
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.apollographql.apollo.coroutines.await
-import com.apollographql.apollo.exception.ApolloException
-//import com.example.inception.GetScheduleQuery
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.example.inception.R
-import com.example.inception.adaptor.ScheduleRecycleViewAdaptor
-import com.example.inception.api.apolloClient
+import com.example.inception.utils.DownloadImageAndSaveToInternalStorage
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.fragment_register.view.*
 import kotlinx.android.synthetic.main.fragment_schedule.view.*
+import java.io.File
 
 class ScheduleFragment : Fragment() {
 
+    var url = "https://fileserver.louisaldorio.site/photo/IMG_5034-555187739.JPG"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        var objectView = inflater.inflate(R.layout.fragment_schedule, container, false)
 
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+        objectView.download_button.setOnClickListener {
+            DownloadImageAndSaveToInternalStorage(requireContext()).execute(url)
+        }
+
+        objectView.view_button.setOnClickListener {
+            readFileFromInternalStorage()
+        }
+
+        return objectView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Picasso.get().load("http://128.199.174.165:8081/photo/DSCF2979-067277033.jpg").into(view.temp)
-
-//        lifecycleScope.launchWhenResumed {
-//            val response = try {
-//                apolloClient(requireContext()).query(GetScheduleQuery()).await()
-//            }catch (e : ApolloException) {
-//                Log.d("Schedule List", "Failure", e)
-//                null
-//            }
-//
-//            val Schedules = response?.data?.schedule_by_user?.filterNotNull()
-//
-//            if (Schedules != null && !response.hasErrors()) {
-//                view.progress_bar_schedule.visibility = View.GONE
-//
-//                val scheduleRv = view.rv_schedule
-//                val adapter = ScheduleRecycleViewAdaptor(Schedules)
-//
-//                scheduleRv.adapter = adapter
-//                scheduleRv.layoutManager = LinearLayoutManager(requireContext())
-//            }else {
-//                Log.i("error schedule", response?.errors?.get(0)?.message.toString())
-//            }
-//        }
-
-        //job Scheduler
+//        Picasso.get().load(url).into(view.temp)
     }
+
+    private fun readFileFromInternalStorage() {
+
+        var mypath = File(requireActivity().filesDir, "Images")
+        mypath = File(mypath,"img.jpg")
+
+        val imageView: ImageView = requireView().findViewById(R.id.temp) as ImageView
+        Log.i("Images",mypath.toString())
+        imageView.setImageDrawable(Drawable.createFromPath(mypath.toString()))
+    }
+
+
+
 }
--- app/src/main/java/com/example/inception/presenter/SupplierPresenter.kt
+++ app/src/main/java/com/example/inception/presenter/SupplierPresenter.kt
@@ -11,34 +11,26 @@ import com.example.inception.data.Supplier
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-//buat sebuah class SupplierPresenter yang menerima 2 parameter yaitu context yang akan dikirim dari parent pemanggil
-//dan sebuah param interface yang telah kita buat sembelumnya.
 class SupplierPresenter(private val context : Context,setView : SupplierInterface) {
-    //simpan interface kedalam variable private
+
     private  var view = setView
-    //buat sebuah variable yang akan menampung list dari data supplier yang akan kita dapatkan dari server
     private var SupplierModel = mutableListOf<Supplier>()
 
-    // dalam class presenter ini, kita akan mengeksekusi logic untuk fetchd ata ke server Graphql
-    //function dibawah menerima sebuah parameter berupa sebuah coroutine yang nantinya akna membuat proses
-    // fetching data ke server bersifat asynchronous
     fun LoadSupplierList(scope: CoroutineScope) {
 
-        //mulai proses saat proses diinvoke
         scope.launch {
-            //lakukan request ke server dan berikan parameter yang sesuai
+
             val response = try {
                 apolloClient(context).query(GetSupplierQuery(role = "Supplier")).await()
             }catch (e: ApolloException){
                 Log.d("SupplierList", "Failure", e)
                 null
             }
-            //data yang dikembalikan ktia simpan terlebih dahulu kedalam variable
+
             val suppliers = response?.data?.users_by_role
 
-            //cek apakah tidak ada error dand ata tidak null
             if(suppliers != null && !response.hasErrors()) {
-                //loop untuk mengisi data yang didapat dari server kedalam model dan disimpan ke variable list yang telah kita buat sebelumnya
+
                 for(item in suppliers){
                     SupplierModel.add(
                         Supplier(
@@ -47,11 +39,13 @@ class SupplierPresenter(private val context : Context,setView : SupplierInterfac
                         )
                     )
                 }
-                //panggil fungsi RenderSupplierList yang kita buat pada interface, kia belum mengoverride function ini
-                // tetapi nanti akan ktia buat
-                // berikan varibale list yang sudah berisi model dari supplier
+
                 view.RenderSupplierList(SupplierModel)
             }
         }
+    }
+
+    private fun writeToCache(){
+
     }
 }
